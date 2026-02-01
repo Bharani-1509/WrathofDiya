81e02d35a6ab9776e307c45dae2e52be1324a4eb
Jaswant B <90975111+JaswantB@users.noreply.github.com>
2026-02-01 12:28:34 +0530

    Updated Player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 5f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
 
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -22,8 +23,14 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = transform.right * x + transform.forward * z;
-        controller.Move(moveDirection * speed * Time.deltaTime);
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // gravity
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
-
